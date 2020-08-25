b710ef4f1e08599ae07b644bd8690454bf8fe2e6
woland-szr <62295565+woland-szr@users.noreply.github.com>
2020-08-25 16:01:21 +0400

    Project

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    entry: path.resolve(__dirname, 'src', 'index.jsx'),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+//    resolve: {
+//        extensions: ['.js', '.jsx']
+//      },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader'],
+            }
+        ],
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'styles.css',
+        }),
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'src', 'index.html'),
+            filename: 'index.html',
+        }),
+        new CleanWebpackPlugin({}),
+    ],
+    devServer: {
+        historyApiFallback: true,
+    }
+};
